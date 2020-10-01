d69cd89f3e8136c23bf9a541c6b242085f0155a4
vanichitkara <vanichitkara18@gmail.com>
2020-10-01 20:20:04 +0530

    Create sum of two numbers.cpp

---
+++ Beginner/04. C++/sum of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int firstNumber, secondNumber;
+	cout<<"Enter first number: ";
+	cin>>firstNumber;
+	cout<<endl<<"Enter second number: ";
+	cin>>secondNumber;
+	int sum=firstNumber+secondNumber;
+	cout<<sum;
+	return 0;
+}
